fbfee6a1658e5a4c3b4a6ec5ad1fa3149b06cdc4
Siddharth1277 <Siddharthvariya07@gmail.com>
2025-04-23 11:51:05 +0530

    Initial commit

--- adminportal/Backend/server.js
+++ adminportal/Backend/server.js
@@ -85,8 +85,8 @@ const uploadNone  = multer().none();
 const db = mysql.createConnection({
   host:     process.env.DB_HOST || 'localhost',
   port:     Number(process.env.DB_PORT) || 3306,
-  user:     process.env.DB_USER || 'amankumar',
-  password: process.env.DB_PASSWORD ||'AK1465$$aman',
+  user:     process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD ||'siddharth',
   database: process.env.DB_NAME || 'Admin_Portal',
   connectTimeout: 10000,
 });
